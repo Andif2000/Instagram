d1e01e8addafb63e70feaf5b88382aef31bcd735
Andi Fahmi <andifahmi266@gmail.com>
2015-07-23 23:00:00 +0300

    Tidy Up

--- Src/Components/Colors.js
+++ Src/Components/Colors.js
@@ -1,15 +1,15 @@
 export const Colors = {
     colorPrimary: '#EC1890',
-    colorSecondary:'#5E52A4',
-    colorPurple:'#4C074D',
+    colorSecondary: '#5E52A4',
+    colorPurple: '#4C074D',
     colorLight: '#F1F1F1',
-    colorWhite:'#FFFFFF',
-    colorBlack:'#000000',
-    colorYellow:'#FFCC15',
-    colorGray:'#808080',
-    colorBrown:'#51143C',
-    colorDark:'#8C8C8C',
-    colorBlue:'#3797EF',
-    colorGray2:'#e9e9e9',
-    colorWhite2:'#F5F5F5'
+    colorWhite: '#FFFFFF',
+    colorYellow: '#FFCC15',
+    colorBlack: '#000000',
+    colorGray: '#808080',
+    colorBrown: '#51143C',
+    colorDark: '#8C8C8C',
+    colorBlue: '#3797EF',
+    colorGray2: '#e9e9e9',
+    colorWhite2: '#F5F5F5'
 }
