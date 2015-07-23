eeb78aff502f4ba541724c1e51d921ce5f4a9b5b
Andi Fahmi <andifahmi266@gmail.com>
2015-07-23 23:00:00 +0300

    Tidy Up

--- Src/Components/BottomTabView.js
+++ Src/Components/BottomTabView.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, ScrollView } from 'react-native'
 import React from 'react'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import Ionicons from 'react-native-vector-icons/Ionicons'
