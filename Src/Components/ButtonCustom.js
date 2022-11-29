e3791540360cc47be608f1e21c4178c10af4dee8
Andif20 <andifahmi26@gmail.com>
2022-11-29 22:19:45 +0800

    Memperbaiki Nama ButtonCustom

--- Src/Components/ButtonCustom.js
+++ Src/Components/ButtonCustom.js
@@ -1,7 +1,7 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
 
-const AgreeButton = (props) => {
+const ButtonCustom = (props) => {
   return (
     <TouchableOpacity onPress={props.onPress} disabled={props.disabled} style={[styles.button, { backgroundColor: props.backgroundColor, width: props.width, paddingVertical: props.height }]}>
       <Text style={[styles.text, { color: props.color, fontSize: props.fontSize, fontWeight: props.bold }]}>{props.title}</Text>
@@ -9,7 +9,7 @@ const AgreeButton = (props) => {
   )
 }
 
-export default AgreeButton
+export default ButtonCustom
 
 const styles = StyleSheet.create({
   button: {
