74c93dd6e53057c98649bbd44cda200c7c7fd428
Andi Fahmi <andifahmi266@gmail.com>
2022-11-30 21:43:06 +0800

    Merapikan Syntax

--- Src/Components/TopBar.js
+++ Src/Components/TopBar.js
@@ -1,19 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Colors } from './Colors'
+import ContainerImage from './ContainerImage'
 
 
 const TopBar = (props) => {
-    if (props.center){
+    if (props.center) {
         return (
             <View style={{ flexDirection: 'row', zIndex: 99, justifyContent: 'center', paddingVertical: 10 }}>
                 <Text style={{ fontSize: 18, color: Colors.colorBlack }}>{props.title}</Text>
             </View>
         )
     }
-    return(
-        <View>
-            
+    return (
+        <View style={{ flexDirection: 'row', zIndex: 99, alignItems: Platform.OS == 'ios' ? 'center' : 'flex-end', paddingVertical: 10 }}>
+            <TouchableOpacity onPress={props.onPress} style={{ position: 'relative', alignItems: 'center', justifyContent: 'center', marginHorizontal: 10, paddingVertical: 5 }}>
+                <ContainerImage size={25} source={require('../Assets/Icons/iconBackB.png')} />
+            </TouchableOpacity>
+            <Text style={{ color: Colors.colorBlack, fontSize: 18, bottom:5 }}>{props.title}</Text>
         </View>
     )
 }
