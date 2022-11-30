6d4efb15c355abda65d45576ab5c49ece80e25ba
Andi Fahmi <andifahmi266@gmail.com>
2022-11-30 21:38:39 +0800

    Membuat Component Container Image

---
+++ Src/Components/ContainerImage.js
@@ -0,0 +1,15 @@
+import { View, Image } from 'react-native'
+import React from 'react'
+
+const ContainerImage = (props) => {
+    let radius = 0
+    if (props.radius) radius = props.size / 2;
+    return (
+        <View style={[{ width: props.width ? props.width : props.size, height: props.height ? props.height : props.size, padding: props.children ? 5 : 0, alignItems: 'center', justifyContent: 'center' }, props.style]}>
+            <Image source={props.source} style={[{ width: props.children ? '60%' : '100%', height: props.children ? '60%' : '100%', resizeMode: 'contain', borderRadius: radius }, props.style]} />
+            {props.children}
+        </View>
+    )
+}
+
+export default ContainerImage
