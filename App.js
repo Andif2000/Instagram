218f9a6f24fa567e148c38843c3649916fd0f779
Andi Fahmi <andifahmi266@gmail.com>
2022-11-30 20:55:44 +0800

    Mengubah route ke Login

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
 import Account from './Src/Screens/Account';
+import Login from './Src/Screens/Login';
 
 export default function App() {
   return (
-   <Account/>
+   <Login/>
   );
 }
 
