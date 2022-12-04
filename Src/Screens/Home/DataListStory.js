bb9e9c733336dfc02742d0833f78dcf76bedf9a4
Andi Fahmi <andifahmi266@gmail.com>
2022-12-04 20:26:01 +0800

    Merubah Syntax Pada DataListHistory

--- Src/Screens/Home/DataListStory.js
+++ Src/Screens/Home/DataListStory.js
@@ -1,42 +1,72 @@
 export const dataStory = [
     {
-        "name" : "Keyla Putri",
-        "photo" : require('../../../assets/Images/ImagesStory/foto1.jpg')
+        "id": "1",
+        "name": "Keyla Putri",
+        "photo": require('../../../assets/Images/ImagesStory/foto1.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto1.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto1.jpg')
     },
     {
-        "name" : "Feby Ardian",
-        "photo" : require('../../../assets/Images/ImagesStory/foto2.jpg')
+        "id": "2",
+        "name": "Feby Ardian",
+        "profile": require('../../../assets/Images/ImagesProfile/foto2.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto2.jpeg'),
+        "post": require('../../../assets/Images/ImagesPost/foto2.jpg')
     },
     {
-        "name" : "Intan Darmawan",
-        "photo" : require('../../../assets/Images/ImagesStory/foto3.jpg')
+        "id": "3",
+        "name": "Intan Darmawan",
+        "profile": require('../../../assets/Images/ImagesProfile/foto3.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto3.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto3.jpg')
     },
     {
-        "name" : "Aynda Viola",
-        "photo" : require('../../../assets/Images/ImagesStory/foto4.jpg')
+        "id": "4",
+        "name": "Aynda Viola",
+        "profile": require('../../../assets/Images/ImagesProfile/foto4.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto4.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto4.jpg')
     },
     {
-        "name" : "Aviva Verliana",
-        "photo" : require('../../../assets/Images/ImagesStory/foto5.jpg')
+        "id": "5",
+        "name": "Aviva Verliana",
+        "profile": require('../../../assets/Images/ImagesProfile/foto5.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto5.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto5.jpg')
     },
     {
-        "name" : "Dewi Fortuna",
-        "photo" : require('../../../assets/Images/ImagesStory/foto6.jpg')
+        "id": "6",
+        "name": "Dewi Fortuna",
+        "profile": require('../../../assets/Images/ImagesProfile/foto6.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto6.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto6.jpg')
     },
     {
-        "name" : "Nurjakia",
-        "photo" : require('../../../assets/Images/ImagesStory/foto7.jpg')
+        "id": "7",
+        "name": "Nurjakia",
+        "profile": require('../../../assets/Images/ImagesProfile/foto7.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto7.jpeg'),
+        "post": require('../../../assets/Images/ImagesPost/foto7.jpg')
     },
     {
-        "name" : "Rosmawati",
-        "photo" : require('../../../assets/Images/ImagesStory/foto8.jpg')
+        "id": "8",
+        "name": "Rosmawati",
+        "profile": require('../../../assets/Images/ImagesProfile/foto8.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto8.png'),
+        "post": require('../../../assets/Images/ImagesPost/foto8.png')
     },
     {
-        "name" : "Sriwahuningsih",
-        "photo" : require('../../../assets/Images/ImagesStory/foto9.jpg')
+        "id": "9",
+        "name": "Sriwahuningsih",
+        "profile": require('../../../assets/Images/ImagesProfile/foto9.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto9.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto9.jpg')
     },
     {
-        "name" : "Izzah Mujahidah",
-        "photo" : require('../../../assets/Images/ImagesStory/foto10.jpg')
+        "id": "10",
+        "name": "Izzah Mujahidah",
+        "profile": require('../../../assets/Images/ImagesProfile/foto10.jpg'),
+        "history": require('../../../assets/Images/ImagesHistory/foto10.jpg'),
+        "post": require('../../../assets/Images/ImagesPost/foto10.jpg')
     },
 ]
