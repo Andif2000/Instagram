83f6e3a14695241f9b34ae6e8901cbe1ee4947c6
Andi Fahmi <andifahmi266@gmail.com>
2022-12-06 01:14:54 +0800

    Membuat File Data Story

---
+++ Src/Components/DataStory.js
@@ -0,0 +1,79 @@
+export const dataStory = [
+    {
+        "id": "0",
+        "name": "Your Story",
+        "profile": require('../../assets/Images/ImagesProfile/foto8.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto8.png'),
+        "post": require('../../assets/Images/ImagesPost/foto8.png')
+    },
+    {
+        "id": "1",
+        "name": "Keyla Putri",
+        "profile": require('../../assets/Images/ImagesProfile/foto1.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto1.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto1.jpg')
+    },
+    {
+        "id": "2",
+        "name": "Feby Ardian",
+        "profile": require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto2.jpeg'),
+        "post": require('../../assets/Images/ImagesPost/foto2.jpg')
+    },
+    {
+        "id": "3",
+        "name": "Intan Darmawan",
+        "profile": require('../../assets/Images/ImagesProfile/foto3.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto3.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto3.jpg')
+    },
+    {
+        "id": "4",
+        "name": "Aynda Viola",
+        "profile": require('../../assets/Images/ImagesProfile/foto4.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto4.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto4.jpg')
+    },
+    {
+        "id": "5",
+        "name": "Aviva Verliana",
+        "profile": require('../../assets/Images/ImagesProfile/foto5.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto5.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto5.jpg')
+    },
+    {
+        "id": "6",
+        "name": "Dewi Fortuna",
+        "profile": require('../../assets/Images/ImagesProfile/foto6.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto6.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto6.jpg')
+    },
+    {
+        "id": "7",
+        "name": "Nurjakia",
+        "profile": require('../../assets/Images/ImagesProfile/foto7.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto7.jpeg'),
+        "post": require('../../assets/Images/ImagesPost/foto7.jpg')
+    },
+    {
+        "id": "8",
+        "name": "Rosmawati",
+        "profile": require('../../assets/Images/ImagesProfile/foto8.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto8.png'),
+        "post": require('../../assets/Images/ImagesPost/foto8.png')
+    },
+    {
+        "id": "9",
+        "name": "Sriwahuningsih",
+        "profile": require('../../assets/Images/ImagesProfile/foto9.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto9.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto9.jpg')
+    },
+    {
+        "id": "10",
+        "name": "Izzah Mujahidah",
+        "profile": require('../../assets/Images/ImagesProfile/foto10.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto10.jpg'),
+        "post": require('../../assets/Images/ImagesPost/foto10.jpg')
+    },
+]
