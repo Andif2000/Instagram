6b0db44d0c5074c16ef3f19ce60aec3fe1e8bf9e
Andi Fahmi <andifahmi266@gmail.com>
2022-12-06 21:16:28 +0800

    Menambahkan file DataPost.js

--- Src/Components/DataPosts.js
+++ Src/Components/DataPosts.js
@@ -3,97 +3,107 @@ export const dataPosts = [
         "id": "1",
         "name": "Nanda Anisa",
         "profile": require('../../assets/Images/ImagesProfile/foto10.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto1.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto1.jpg'),
         "like": 989,
         "isLiked": false,
-        "captions": "“Cobalah jadi malam agar kau tahu rasanya rindu, dan jadilah senja sesekali agar kau tahu artinya menanti.”"
+        "captions": "“Cobalah jadi malam agar kau tahu rasanya rindu, dan jadilah senja sesekali agar kau tahu artinya menanti.”",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "2",
         "name": "Muya Gustiawati",
         "profile": require('../../assets/Images/ImagesProfile/foto8.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto2.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto2.jpg'),
         "like": 775,
         "isLiked": false,
-        "captions": "Senja mengajarkan pada kita, bahwa kehidupan tak selalu berjalan dengan cemerlang dan bersinar"
+        "captions": "Senja mengajarkan pada kita, bahwa kehidupan tak selalu berjalan dengan cemerlang dan bersinar",
+        "time" : "3 hours ago"
     },
     {
         "id": "3",
         "name": "Rifandi Budiawan",
         "profile": require('../../assets/Images/ImagesProfile/foto6.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto3.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto3.jpg'),
         "like": 646,
         "isLiked": false,
-        "captions": "Keinginan yang lebih mulia dari manusia dilayani oleh alam, yaitu cinta akan keindahan."
+        "captions": "Keinginan yang lebih mulia dari manusia dilayani oleh alam, yaitu cinta akan keindahan.",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "4",
         "name": "Sahrul",
         "profile": require('../../assets/Images/ImagesProfile/foto4.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto4.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto4.jpg'),
         "like": 654,
         "isLiked": false,
-        "captions": "Saya rindu waktu kecil dimana sebuah ban bekas bisa membuat hari yang lebih ceria dibandingkan bermain gadget  "
+        "captions": "Saya rindu waktu kecil dimana sebuah ban bekas bisa membuat hari yang lebih ceria dibandingkan bermain gadget  ",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "5",
         "name": "Elsa Rofika",
         "profile": require('../../assets/Images/ImagesProfile/foto2.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto5.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto5.jpg'),
         "like": 467,
         "isLiked": false,
-        "captions": "Saya kira kesenangan hidup pedesaan benar-benar terletak pada bukti-bukti yang diperbarui secara abadi dari tekad untuk hidup."
+        "captions": "Saya kira kesenangan hidup pedesaan benar-benar terletak pada bukti-bukti yang diperbarui secara abadi dari tekad untuk hidup.",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "6",
         "name": "Asniar Tamil",
         "profile": require('../../assets/Images/ImagesProfile/foto1.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto6.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto6.jpg'),
         "like": 422,
         "isLiked": false,
-        "captions": "Teman baik membuat saat-saat indah menjadi lebih baik dan masa-masa sulit menjadi lebih mudah"
+        "captions": "Teman baik membuat saat-saat indah menjadi lebih baik dan masa-masa sulit menjadi lebih mudah",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "7",
         "name": "Wildayanti",
         "profile": require('../../assets/Images/ImagesProfile/foto3.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto7.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto7.jpg'),
         "like": 423,
         "isLiked": false,
-        "captions": "Hidup itu harus santai, semangat dan jangan mudah putus asa jika menghadapi permasalahan yang rumit."
+        "captions": "Hidup itu harus santai, semangat dan jangan mudah putus asa jika menghadapi permasalahan yang rumit.",
+        "time" : "3 hours ago"
     },
     {
         "id": "8",
         "name": "Nurul Azizah",
         "profile": require('../../assets/Images/ImagesProfile/foto5.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto8.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto8.jpg'),
         "like": 233,
         "isLiked": false,
-        "captions": "Mungkin patah hatimu itu adalah cara Tuhan untuk mengatakan 'kembalilah kamu sudah pergi terlalu jauh, aku rindu mendengar doa-doamu"
+        "captions": "Mungkin patah hatimu itu adalah cara Tuhan untuk mengatakan 'kembalilah kamu sudah pergi terlalu jauh, aku rindu mendengar doa-doamu",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "9",
         "name": "Elvi Emisasri",
         "profile": require('../../assets/Images/ImagesProfile/foto7.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto9.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto9.jpg'),
         "like": 423,
         "isLiked": false,
-        "captions": "Ada perasaan rindu di separuh malam yang sepi, saat semua orang sedang asyik terlelap."
+        "captions": "Ada perasaan rindu di separuh malam yang sepi, saat semua orang sedang asyik terlelap.",
+        "time" : "3 hours ago"
 
     },
     {
         "id": "10",
         "name": "Amirah",
         "profile": require('../../assets/Images/ImagesProfile/foto9.jpg'),
-        "post": require('../../assets/Images/ImagePosts/foto10.jpeg'),
+        "post": require('../../assets/Images/ImagesPosts/foto10.jpeg'),
         "like": 539,
         "isLiked": false,
-        "captions": "Langit adalah film yang tak terbatas bagiku. Aku tidak pernah bosan melihat apa yang terjadi di sana"
+        "captions": "Langit adalah film yang tak terbatas bagiku. Aku tidak pernah bosan melihat apa yang terjadi di sana",
+        "time" : "3 hours ago"
     },
 ]
