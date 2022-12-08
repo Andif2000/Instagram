7b22363603af7e387cefa60fcfa86379e9da7e23
Andi Fahmi <andifahmi266@gmail.com>
2022-12-08 20:02:10 +0800

    Menambahkan Component SearchContent

---
+++ Src/Components/SearchContent.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchContent = () => {
+  return (
+    <View>
+      <Text>SearchContent</Text>
+    </View>
+  )
+}
+
+export default SearchContent
