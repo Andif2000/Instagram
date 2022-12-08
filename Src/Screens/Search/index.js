11dd0a00e8c95431316ef51385739bdaa862ea49
Andi Fahmi <andifahmi266@gmail.com>
2022-12-08 19:23:32 +0800

    Mengimpor SearchBox Ke Screen Search

--- Src/Screens/Search/index.js
+++ Src/Screens/Search/index.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+
+import SearchBox from '../../Components/SearchBox'
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#ffffff',
+        position: 'relative'
+      }}>
+      <ScrollView>
+        <SearchBox />
+      </ScrollView>
     </View>
   )
 }
