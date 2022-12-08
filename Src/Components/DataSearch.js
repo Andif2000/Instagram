729a47521be6e4f6039fdbbe8bfc407afd5e4b06
Andi Fahmi <andifahmi266@gmail.com>
2022-12-08 20:02:33 +0800

    Menambahkan Component DataSearch

---
+++ Src/Components/DataSearch.js
@@ -0,0 +1,13 @@
+export const DataSearch = [
+    {
+        id:0,
+        images:[
+            require('../../assets/Images/ImagesPosts/foto1.jpg'),
+            require('../../assets/Images/ImagesPosts/foto2.jpg'),
+            require('../../assets/Images/ImagesStory/foto1.jpg'),
+            require('../../assets/Images/ImagesStory/foto2.jpeg'),
+            require('../../assets/Images/ImagesPosts/foto3.jpg'),
+            require('../../assets/Images/ImagesStory/foto3.jpg'),
+        ]
+    },
+]
