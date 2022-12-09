95a19104f4e340c697e21e8c386b83250ca5dfbd
Andi Fahmi <andifahmi266@gmail.com>
2022-12-09 23:02:54 +0800

    Merapikan Syntax Componen Top Bar

--- Src/Components/TopBar.js
+++ Src/Components/TopBar.js
@@ -7,17 +7,40 @@ import ContainerImage from './ContainerImage'
 const TopBar = (props) => {
     if (props.center) {
         return (
-            <View style={{ flexDirection: 'row', zIndex: 99, justifyContent: 'center', paddingVertical: 10 }}>
-                <Text style={{ fontSize: 18, color: Colors.colorBlack }}>{props.title}</Text>
+            <View
+                style={{
+                    flexDirection: 'row',
+                    zIndex: 99,
+                    justifyContent: 'center',
+                    paddingVertical: 10
+                }}>
+                <Text
+                    style={{ fontSize: 18, color: Colors.colorBlack }}>{props.title}</Text>
             </View>
         )
     }
     return (
-        <View style={{ flexDirection: 'row', zIndex: 99, alignItems: Platform.OS == 'ios' ? 'center' : 'flex-end', paddingVertical: 10 }}>
-            <TouchableOpacity onPress={props.onPress} style={{ position: 'relative', alignItems: 'center', justifyContent: 'center', marginHorizontal: 10, paddingVertical: 5 }}>
-                <ContainerImage size={25} source={require('../Assets/Icons/iconBackB.png')} />
+        <View
+            style={{
+                flexDirection: 'row',
+                zIndex: 99,
+                alignItems: Platform.OS == 'ios' ? 'center' : 'flex-end',
+                paddingVertical: 10
+            }}>
+            <TouchableOpacity
+                onPress={props.onPress}
+                style={{
+                    position: 'relative',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginHorizontal: 10,
+                    paddingVertical: 5
+                }}>
+                <ContainerImage
+                    size={25}
+                    source={require('../Assets/Icons/iconBackB.png')} />
             </TouchableOpacity>
-            <Text style={{ color: Colors.colorBlack, fontSize: 18, bottom:5 }}>{props.title}</Text>
+            <Text style={{ color: Colors.colorBlack, fontSize: 18, bottom: 5 }}>{props.title}</Text>
         </View>
     )
 }
