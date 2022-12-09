ad6f64a266e1afad47ee9f3f23a5838c9c809cb0
Andi Fahmi <andifahmi266@gmail.com>
2022-12-09 23:02:36 +0800

    Merapikan Syntax Componen Container Image

--- Src/Components/ContainerImage.js
+++ Src/Components/ContainerImage.js
@@ -5,8 +5,23 @@ const ContainerImage = (props) => {
     let radius = 0
     if (props.radius) radius = props.size / 2;
     return (
-        <View style={[{ width: props.width ? props.width : props.size, height: props.height ? props.height : props.size, padding: props.children ? 5 : 0, alignItems: 'center', justifyContent: 'center' }, props.style]}>
-            <Image source={props.source} style={[{ width: props.children ? '60%' : '100%', height: props.children ? '60%' : '100%', resizeMode: 'contain', borderRadius: radius }, props.style]} />
+        <View
+            style={[{
+                width: props.width ? props.width : props.size,
+                height: props.height ? props.height : props.size,
+                padding: props.children ? 5 : 0,
+                alignItems: 'center',
+                justifyContent: 'center'
+            },
+            props.style]}>
+            <Image source={props.source}
+                style={[{
+                    width: props.children ? '60%' : '100%',
+                    height: props.children ? '60%' : '100%',
+                    resizeMode: 'contain',
+                    borderRadius: radius
+                },
+                props.style]} />
             {props.children}
         </View>
     )
