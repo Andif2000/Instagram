d6b4bbcb3a1df691921977444a912373e83cc662
Andi Fahmi <andifahmi266@gmail.com>
2022-12-11 22:03:51 +0800

    Menambahkan Componen DataReels

---
+++ Src/Components/DataReels.js
@@ -0,0 +1,47 @@
+export const videoData = [
+    {
+        person: 'Abdi Maulana',
+        video : require('../../assets/Videos/Reels/video1.mp4'),
+        title: 'Beautiful Sun Scene',
+        description: 'Alhamdulillah Masih Bisa Menyaksikan CiptaanMu',
+        music : 'Aku Tak Tahu.Mp3',
+        likes: '402k',
+        isLike : false
+    },
+    {
+        person: 'Elhau Naldy',
+        video : require('../../assets/Videos/Reels/video2.mp4'),
+        title: 'Banyak Cara Melepas Penat',
+        description: 'Salah Satu Cara Melepas Lelah Ini',
+        music : 'Masih Belum Tahu.Mp3',
+        likes: '2000k',
+        isLike : false
+    },
+    {
+        person: 'Dhanee',
+        video : require('../../assets/Videos/Reels/video3.mp4'),
+        title: 'Menikmati Ini Adalah Nikmat Yang Luar Biasa',
+        description: 'Thankss',
+        music : 'Ku Mencari Tahu.Mp3',
+        likes: '988k',
+        isLike : false
+    },
+    {
+        person: 'Mahardika P.',
+        video : require('../../assets/Videos/Reels/video4.mp4'),
+        title: 'Berjalan Tanpa Tujuan',
+        description: 'Kemanapun Asal Bersamamu',
+        music : 'Tapi Tak Tahu.Mp3',
+        likes: '4321k',
+        isLike : false
+    },
+    {
+        person: 'Andif20',
+        video : require('../../assets/Videos/Reels/video5.mp4'),
+        title: 'Begini Mo Dulu',
+        description: 'Nantipi Lagi Brgitu',
+        music : 'Tahu.Mp3',
+        likes: '2006k',
+        isLike : false
+    }
+]
