37fb6c9d52f2d50c25805675382135ab25bab4a8
Andi Fahmi <andifahmi266@gmail.com>
2022-12-13 14:10:14 +0800

    Membuat Componen Dtabase Untuk Memudahkan Akses Data

---
+++ Src/Components/DataBase.js
@@ -0,0 +1,277 @@
+export const dataPosts = [
+    {
+        "id": "1",
+        "name": "Nanda Anisa",
+        "profile": require('../../assets/Images/ImagesProfile/foto10.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto1.jpg'),
+        "like": 989,
+        "isLiked": false,
+        "captions": "“Cobalah jadi malam agar kau tahu rasanya rindu, dan jadilah senja sesekali agar kau tahu artinya menanti.”",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "2",
+        "name": "Muya Gustiawati",
+        "profile": require('../../assets/Images/ImagesProfile/foto8.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto2.jpg'),
+        "like": 775,
+        "isLiked": false,
+        "captions": "Senja mengajarkan pada kita, bahwa kehidupan tak selalu berjalan dengan cemerlang dan bersinar",
+        "time" : "3 hours ago"
+    },
+    {
+        "id": "3",
+        "name": "Rifandi Budiawan",
+        "profile": require('../../assets/Images/ImagesProfile/foto6.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto3.jpg'),
+        "like": 646,
+        "isLiked": false,
+        "captions": "Keinginan yang lebih mulia dari manusia dilayani oleh alam, yaitu cinta akan keindahan.",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "4",
+        "name": "Sahrul",
+        "profile": require('../../assets/Images/ImagesProfile/foto4.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto4.jpg'),
+        "like": 654,
+        "isLiked": false,
+        "captions": "Saya rindu waktu kecil dimana sebuah ban bekas bisa membuat hari yang lebih ceria dibandingkan bermain gadget  ",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "5",
+        "name": "Elsa Rofika",
+        "profile": require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto5.jpg'),
+        "like": 467,
+        "isLiked": false,
+        "captions": "Saya kira kesenangan hidup pedesaan benar-benar terletak pada bukti-bukti yang diperbarui secara abadi dari tekad untuk hidup.",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "6",
+        "name": "Asniar Tamil",
+        "profile": require('../../assets/Images/ImagesProfile/foto1.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto6.jpg'),
+        "like": 422,
+        "isLiked": false,
+        "captions": "Teman baik membuat saat-saat indah menjadi lebih baik dan masa-masa sulit menjadi lebih mudah",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "7",
+        "name": "Wildayanti",
+        "profile": require('../../assets/Images/ImagesProfile/foto3.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto7.jpg'),
+        "like": 423,
+        "isLiked": false,
+        "captions": "Hidup itu harus santai, semangat dan jangan mudah putus asa jika menghadapi permasalahan yang rumit.",
+        "time" : "3 hours ago"
+    },
+    {
+        "id": "8",
+        "name": "Nurul Azizah",
+        "profile": require('../../assets/Images/ImagesProfile/foto5.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto8.jpg'),
+        "like": 233,
+        "isLiked": false,
+        "captions": "Mungkin patah hatimu itu adalah cara Tuhan untuk mengatakan 'kembalilah kamu sudah pergi terlalu jauh, aku rindu mendengar doa-doamu",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "9",
+        "name": "Elvi Emisasri",
+        "profile": require('../../assets/Images/ImagesProfile/foto7.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto9.jpg'),
+        "like": 423,
+        "isLiked": false,
+        "captions": "Ada perasaan rindu di separuh malam yang sepi, saat semua orang sedang asyik terlelap.",
+        "time" : "3 hours ago"
+
+    },
+    {
+        "id": "10",
+        "name": "Amirah",
+        "profile": require('../../assets/Images/ImagesProfile/foto9.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto10.jpeg'),
+        "like": 539,
+        "isLiked": false,
+        "captions": "Langit adalah film yang tak terbatas bagiku. Aku tidak pernah bosan melihat apa yang terjadi di sana",
+        "time" : "3 hours ago"
+    },
+]
+
+export const dataVideo = [
+    {
+        person: 'Abdi Maulana',
+        profile : require('../../assets/Images/ImagesProfile/foto1.jpg'),
+        video : require('../../assets/Videos/Reels/video1.mp4'),
+        title: 'Beautiful Sun Scene',
+        description: 'Alhamdulillah Masih Bisa Menyaksikan CiptaanMu',
+        music : 'Aku Tak Tahu.Mp3',
+        likes: '402k',
+        isLike : false
+    },
+    {
+        person: 'Elhau Naldy',
+        profile : require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        video : require('../../assets/Videos/Reels/video2.mp4'),
+        title: 'Banyak Cara Melepas Penat',
+        description: 'Salah Satu Cara Melepas Lelah Ini',
+        music : 'Masih Belum Tahu.Mp3',
+        likes: '2000k',
+        isLike : false
+    },
+    {
+        person: 'Dhanee',
+        profile : require('../../assets/Images/ImagesProfile/foto3.jpg'),
+        video : require('../../assets/Videos/Reels/video3.mp4'),
+        title: 'Menikmati Ini Adalah Nikmat Yang Luar Biasa',
+        description: 'Thankss',
+        music : 'Ku Mencari Tahu.Mp3',
+        likes: '988k',
+        isLike : false
+    },
+    {
+        person: 'Mahardika P.',
+        profile : require('../../assets/Images/ImagesProfile/foto4.jpg'),
+        video : require('../../assets/Videos/Reels/video4.mp4'),
+        title: 'Berjalan Tanpa Tujuan',
+        description: 'Kemanapun Asal Bersamamu',
+        music : 'Tapi Tak Tahu.Mp3',
+        likes: '4321k',
+        isLike : false
+    },
+    {
+        person: 'Andif20',
+        profile : require('../../assets/Images/ImagesProfile/foto5.jpg'),
+        video : require('../../assets/Videos/Reels/video5.mp4'),
+        title: 'Begini Mo Dulu',
+        description: 'Nantipi Lagi Begitu',
+        music : 'Tahu.Mp3',
+        likes: '2006k',
+        isLike : false
+    }
+]
+
+export const dataSearch = [
+    {
+        id:0,
+        images:[
+            require('../../assets/Images/ImagesPosts/foto1.jpg'),
+            require('../../assets/Images/ImagesPosts/foto2.jpg'),
+            require('../../assets/Images/ImagesStory/foto1.jpg'),
+            require('../../assets/Images/ImagesStory/foto2.jpeg'),
+            require('../../assets/Images/ImagesPosts/foto3.jpg'),
+            require('../../assets/Images/ImagesStory/foto3.jpg'),
+        ]
+    },
+    {
+        id:1,
+        images:[
+            require('../../assets/Images/ImagesPosts/foto4.jpg'),
+            require('../../assets/Images/ImagesPosts/foto5.jpg'),
+            require('../../assets/Images/ImagesStory/foto4.jpg'),
+            require('../../assets/Images/ImagesStory/foto5.jpg'),
+            require('../../assets/Images/ImagesPosts/foto6.jpg'),
+        ]
+    },
+    {
+        id:2,
+        images:[
+            require('../../assets/Images/ImagesPosts/foto7.jpg'),
+            require('../../assets/Images/ImagesPosts/foto8.jpg'),
+            require('../../assets/Images/ImagesStory/foto7.jpeg'),
+            require('../../assets/Images/ImagesStory/foto8.png'),
+            require('../../assets/Images/ImagesPosts/foto9.jpg'),
+            require('../../assets/Images/ImagesStory/foto9.jpg'),
+        ]
+    },
+]
+
+export const dataStory = [
+    {
+        "id": "0",
+        "name": "Your Story",
+        "profile": require('../../assets/Images/ImagesProfile/foto8.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto8.png'),
+        "post": require('../../assets/Images/ImagesPosts/foto8.jpg')
+    },
+    {
+        "id": "1",
+        "name": "Keyla Putri",
+        "profile": require('../../assets/Images/ImagesProfile/foto1.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto1.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto1.jpg')
+    },
+    {
+        "id": "2",
+        "name": "Feby Ardian",
+        "profile": require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto2.jpeg'),
+        "post": require('../../assets/Images/ImagesPosts/foto2.jpg')
+    },
+    {
+        "id": "3",
+        "name": "Intan Darmawan",
+        "profile": require('../../assets/Images/ImagesProfile/foto3.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto3.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto3.jpg')
+    },
+    {
+        "id": "4",
+        "name": "Aynda Viola",
+        "profile": require('../../assets/Images/ImagesProfile/foto4.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto4.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto4.jpg')
+    },
+    {
+        "id": "5",
+        "name": "Aviva Verliana",
+        "profile": require('../../assets/Images/ImagesProfile/foto5.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto5.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto5.jpg')
+    },
+    {
+        "id": "6",
+        "name": "Dewi Fortuna",
+        "profile": require('../../assets/Images/ImagesProfile/foto6.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto6.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto6.jpg')
+    },
+    {
+        "id": "7",
+        "name": "Nurjakia",
+        "profile": require('../../assets/Images/ImagesProfile/foto7.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto7.jpeg'),
+        "post": require('../../assets/Images/ImagesPosts/foto7.jpg')
+    },
+    {
+        "id": "8",
+        "name": "Rosmawati",
+        "profile": require('../../assets/Images/ImagesProfile/foto8.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto8.png'),
+        "post": require('../../assets/Images/ImagesPosts/foto8.jpg')
+    },
+    {
+        "id": "9",
+        "name": "Sriwahuningsih",
+        "profile": require('../../assets/Images/ImagesProfile/foto9.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto9.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto9.jpg')
+    },
+    {
+        "id": "10",
+        "name": "Izzah Mujahidah",
+        "profile": require('../../assets/Images/ImagesProfile/foto10.jpg'),
+        "story": require('../../assets/Images/ImagesStory/foto10.jpg'),
+        "post": require('../../assets/Images/ImagesPosts/foto10.jpeg')
+    },
+]
