b855d7c0e70ec17fef27d271f86b1299fb0f42ff
Andi Fahmi <andifahmi266@gmail.com>
2022-12-14 13:56:26 +0800

    MMenambahkan Data Untuk Screen Activity

--- Src/Components/DataBase.js
+++ Src/Components/DataBase.js
@@ -1,3 +1,4 @@
+//Posts Data
 export const dataPosts = [
     {
         "id": "1",
@@ -7,7 +8,7 @@ export const dataPosts = [
         "like": 989,
         "isLiked": false,
         "captions": "“Cobalah jadi malam agar kau tahu rasanya rindu, dan jadilah senja sesekali agar kau tahu artinya menanti.”",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -18,7 +19,7 @@ export const dataPosts = [
         "like": 775,
         "isLiked": false,
         "captions": "Senja mengajarkan pada kita, bahwa kehidupan tak selalu berjalan dengan cemerlang dan bersinar",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
     },
     {
         "id": "3",
@@ -28,7 +29,7 @@ export const dataPosts = [
         "like": 646,
         "isLiked": false,
         "captions": "Keinginan yang lebih mulia dari manusia dilayani oleh alam, yaitu cinta akan keindahan.",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -39,7 +40,7 @@ export const dataPosts = [
         "like": 654,
         "isLiked": false,
         "captions": "Saya rindu waktu kecil dimana sebuah ban bekas bisa membuat hari yang lebih ceria dibandingkan bermain gadget  ",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -50,7 +51,7 @@ export const dataPosts = [
         "like": 467,
         "isLiked": false,
         "captions": "Saya kira kesenangan hidup pedesaan benar-benar terletak pada bukti-bukti yang diperbarui secara abadi dari tekad untuk hidup.",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -61,7 +62,7 @@ export const dataPosts = [
         "like": 422,
         "isLiked": false,
         "captions": "Teman baik membuat saat-saat indah menjadi lebih baik dan masa-masa sulit menjadi lebih mudah",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -72,7 +73,7 @@ export const dataPosts = [
         "like": 423,
         "isLiked": false,
         "captions": "Hidup itu harus santai, semangat dan jangan mudah putus asa jika menghadapi permasalahan yang rumit.",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
     },
     {
         "id": "8",
@@ -82,7 +83,7 @@ export const dataPosts = [
         "like": 233,
         "isLiked": false,
         "captions": "Mungkin patah hatimu itu adalah cara Tuhan untuk mengatakan 'kembalilah kamu sudah pergi terlalu jauh, aku rindu mendengar doa-doamu",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -93,7 +94,7 @@ export const dataPosts = [
         "like": 423,
         "isLiked": false,
         "captions": "Ada perasaan rindu di separuh malam yang sepi, saat semua orang sedang asyik terlelap.",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
 
     },
     {
@@ -104,67 +105,69 @@ export const dataPosts = [
         "like": 539,
         "isLiked": false,
         "captions": "Langit adalah film yang tak terbatas bagiku. Aku tidak pernah bosan melihat apa yang terjadi di sana",
-        "time" : "3 hours ago"
+        "time": "3 hours ago"
     },
 ]
 
+//Videos Data
 export const dataVideo = [
     {
         person: 'Abdi Maulana',
-        profile : require('../../assets/Images/ImagesProfile/foto1.jpg'),
-        video : require('../../assets/Videos/Reels/video1.mp4'),
+        profile: require('../../assets/Images/ImagesProfile/foto1.jpg'),
+        video: require('../../assets/Videos/Reels/video1.mp4'),
         title: 'Beautiful Sun Scene',
         description: 'Alhamdulillah Masih Bisa Menyaksikan CiptaanMu',
-        music : 'Aku Tak Tahu.Mp3',
+        music: 'Aku Tak Tahu.Mp3',
         likes: '402k',
-        isLike : false
+        isLike: false
     },
     {
         person: 'Elhau Naldy',
-        profile : require('../../assets/Images/ImagesProfile/foto2.jpg'),
-        video : require('../../assets/Videos/Reels/video2.mp4'),
+        profile: require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        video: require('../../assets/Videos/Reels/video2.mp4'),
         title: 'Banyak Cara Melepas Penat',
         description: 'Salah Satu Cara Melepas Lelah Ini',
-        music : 'Masih Belum Tahu.Mp3',
+        music: 'Masih Belum Tahu.Mp3',
         likes: '2000k',
-        isLike : false
+        isLike: false
     },
     {
         person: 'Dhanee',
-        profile : require('../../assets/Images/ImagesProfile/foto3.jpg'),
-        video : require('../../assets/Videos/Reels/video3.mp4'),
+        profile: require('../../assets/Images/ImagesProfile/foto3.jpg'),
+        video: require('../../assets/Videos/Reels/video3.mp4'),
         title: 'Menikmati Ini Adalah Nikmat Yang Luar Biasa',
         description: 'Thankss',
-        music : 'Ku Mencari Tahu.Mp3',
+        music: 'Ku Mencari Tahu.Mp3',
         likes: '988k',
-        isLike : false
+        isLike: false
     },
     {
         person: 'Mahardika P.',
-        profile : require('../../assets/Images/ImagesProfile/foto4.jpg'),
-        video : require('../../assets/Videos/Reels/video4.mp4'),
+        profile: require('../../assets/Images/ImagesProfile/foto4.jpg'),
+        video: require('../../assets/Videos/Reels/video4.mp4'),
         title: 'Berjalan Tanpa Tujuan',
         description: 'Kemanapun Asal Bersamamu',
-        music : 'Tapi Tak Tahu.Mp3',
+        music: 'Tapi Tak Tahu.Mp3',
         likes: '4321k',
-        isLike : false
+        isLike: false
     },
     {
         person: 'Andif20',
-        profile : require('../../assets/Images/ImagesProfile/foto5.jpg'),
-        video : require('../../assets/Videos/Reels/video5.mp4'),
+        profile: require('../../assets/Images/ImagesProfile/foto5.jpg'),
+        video: require('../../assets/Videos/Reels/video5.mp4'),
         title: 'Begini Mo Dulu',
         description: 'Nantipi Lagi Begitu',
-        music : 'Tahu.Mp3',
+        music: 'Tahu.Mp3',
         likes: '2006k',
-        isLike : false
+        isLike: false
     }
 ]
 
+//Searchs Data
 export const dataSearch = [
     {
-        id:0,
-        images:[
+        id: 0,
+        images: [
             require('../../assets/Images/ImagesPosts/foto1.jpg'),
             require('../../assets/Images/ImagesPosts/foto2.jpg'),
             require('../../assets/Images/ImagesStory/foto1.jpg'),
@@ -174,8 +177,8 @@ export const dataSearch = [
         ]
     },
     {
-        id:1,
-        images:[
+        id: 1,
+        images: [
             require('../../assets/Images/ImagesPosts/foto4.jpg'),
             require('../../assets/Images/ImagesPosts/foto5.jpg'),
             require('../../assets/Images/ImagesStory/foto4.jpg'),
@@ -184,8 +187,8 @@ export const dataSearch = [
         ]
     },
     {
-        id:2,
-        images:[
+        id: 2,
+        images: [
             require('../../assets/Images/ImagesPosts/foto7.jpg'),
             require('../../assets/Images/ImagesPosts/foto8.jpg'),
             require('../../assets/Images/ImagesStory/foto7.jpeg'),
@@ -196,6 +199,7 @@ export const dataSearch = [
     },
 ]
 
+//Stories Data
 export const dataStory = [
     {
         "id": "0",
@@ -275,3 +279,71 @@ export const dataStory = [
         "post": require('../../assets/Images/ImagesPosts/foto10.jpeg')
     },
 ]
+
+//Friends Profile Data
+export const dataFriendsProfile = [
+    {
+        name: 'Molina',
+        accountName: 'Molinaae',
+        profileImage: require('../../assets/Images/ImagesProfile/foto5.jpg'),
+        post: 24,
+        folloewrs: 649,
+        following: 399,
+        follow: false
+    },
+    {
+        name: 'Windy Chan',
+        accountName: 'Whiican',
+        profileImage: require('../../assets/Images/ImagesProfile/foto3.jpg'),
+        post: 241,
+        folloewrs: 2514,
+        following: 1002,
+        follow: false
+    },
+    {
+        name: 'Feby Anisa',
+        accountName: 'Feby_Chan',
+        profileImage: require('../../assets/Images/ImagesProfile/foto7.jpg'),
+        post: 54,
+        folloewrs: 3423,
+        following: 3332,
+        follow: false
+    },
+    {
+        name: 'Markonah',
+        accountName: 'Mrkonh',
+        profileImage: require('../../assets/Images/ImagesProfile/foto9.jpg'),
+        post: 53,
+        folloewrs: 1025,
+        following: 987,
+        follow: false
+    },
+    {
+        name: 'Wildayanti',
+        accountName: 'Wil_thy',
+        profileImage: require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        post: 24,
+        folloewrs: 649,
+        following: 399,
+        follow: false
+    },
+    {
+        name: 'Fikri M. Adbi',
+        accountName: 'Fik_MA',
+        profileImage: require('../../assets/Images/ImagesProfile/foto2.jpg'),
+        post: 78,
+        folloewrs: 2011,
+        following: 1029,
+        follow: false
+    },
+    {
+        name: 'Mujahidah Husnaeni',
+        accountName: 'Iddah_En',
+        profileImage: require('../../assets/Images/ImagesProfile/foto8.jpg'),
+        post: 75,
+        folloewrs: 3323,
+        following: 2112,
+        follow: false
+    },
+]
+
