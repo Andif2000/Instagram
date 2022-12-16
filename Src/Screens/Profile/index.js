a4ddf62fdc09cf9554a83677d7f9afe925c55aa9
Andi Fahmi <andifahmi266@gmail.com>
2022-12-17 07:12:35 +0800

    Mulai Mengerjakan Screen Profile

--- Src/Screens/Profile/index.js
+++ Src/Screens/Profile/index.js
@@ -1,10 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { ProfileBody, ProfileButtons } from '../../Components/ProfileBody'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Next make profile </Text>
+    <View style={{ width: '100%', height: '100%', backgroundColor: '#ffffff' }}>
+      <View style={{ width: '100%', padding: 10 }}>
+        <ProfileBody
+          name='Andi Fahmi'
+          accountName='Andif20'
+          profileImage={require('../../../assets/Images/ImagesProfile/foto3.jpg')}
+          followers='2.5M'
+          following='538'
+          post='20' />
+        <ProfileButtons
+          id={0}
+          name='Andi Fahmi'
+          accountName='Andif20'
+          profileImage={require('../../../assets/Images/ImagesProfile/foto3.jpg')}
+        />
+      </View>
     </View>
   )
 }
