7e82bb47cd8bc6f8b247fe1c04517f188800d913
Andi Fahmi <andifahmi266@gmail.com>
2022-12-19 13:05:35 +0800

    Menambahkan Story Highlights Pada Screen Profile

--- Src/Screens/Profile/index.js
+++ Src/Screens/Profile/index.js
@@ -1,8 +1,52 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { ProfileBody, ProfileButtons } from '../../Components/ProfileBody'
+import Entypo from 'react-native-vector-icons/Entypo'
 
 const Profile = () => {
+  const circuls = [];
+  const numberOfCircels = 10;
+
+  for (let index = 0; index < numberOfCircels; index++) {
+    circuls.push(
+      <View key={index}>
+        {index === 0 ?
+          (
+            <TouchableOpacity
+              style={{
+                width: 60,
+                height: 60,
+                borderRadius: 100,
+                borderWidth: 1,
+                opacity: 0.7,
+                marginHorizontal: 5,
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}>
+              <View>
+                <Entypo name='plus' style={{ fontSize: 40, color: '#000000' }} />
+              </View>
+            </TouchableOpacity>
+          ) :
+          (
+            <TouchableOpacity
+              style={{
+                width: 60,
+                height: 60,
+                borderRadius: 100,
+                backgroundColor: '#000000',
+                opacity: 0.1,
+                marginHorizontal: 5
+              }}>
+              <View>
+
+              </View>
+            </TouchableOpacity>
+          )}
+      </View>
+    )
+
+  }
   return (
     <View style={{ width: '100%', height: '100%', backgroundColor: '#ffffff' }}>
       <View style={{ width: '100%', padding: 10 }}>
@@ -19,6 +63,18 @@ const Profile = () => {
           accountName='Andif20'
           profileImage={require('../../../assets/Images/ImagesProfile/foto3.jpg')}
         />
+      </View>
+      <View>
+        <Text
+          style={{ padding: 10, letterSpacing: 1, fontSize: 14 }}>
+          Story  Highlights
+        </Text>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={{ paddingVertical: 5, paddingHorizontal: 10 }}>
+          {circuls}
+        </ScrollView>
       </View>
     </View>
   )
