f32aee19c6bcc865d60d4641cdd7b0608aca7412
Andi Fahmi <andifahmi266@gmail.com>
2022-12-20 05:38:06 +0800

    Menambahkan Componen BottomTaView

---
+++ Src/Components/BottomTabView.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
+
+const BottomTabView = () => {
+  return (
+    <View>
+      <Text>BottomTabView</Text>
+    </View>
+  )
+}
+
+export default BottomTabView
