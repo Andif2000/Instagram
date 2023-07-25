062a4686793a93cc7dbd72b0ebc79ce90f6668a3
Andi Fahmi <andifahmi266@gmail.com>
2023-07-25 23:00:00 +0300

    Tidy Up

--- Src/Components/FriendProfile.js
+++ Src/Components/FriendProfile.js
@@ -4,7 +4,6 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 import Feather from 'react-native-vector-icons/Feather'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import { useNavigation } from '@react-navigation/native'
-
 import { ProfileBody, ProfileButtons } from './ProfileBody'
 import { dataFriendsProfile } from './DataBase'
 
