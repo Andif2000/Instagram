899b1e16b078ea210800560d12c4d7ea7ac188da
Andi Fahmi <andifahmi266@gmail.com>
2023-07-27 08:26:44 +0800

    Update Button Custom

--- Src/Components/ButtonCustom.js
+++ Src/Components/ButtonCustom.js
@@ -1,35 +1,43 @@
-import { Text, TouchableOpacity, StyleSheet } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const ButtonCustom = (props) => {
   return (
     <TouchableOpacity
       onPress={props.onPress}
       disabled={props.disabled}
-      style={[styles.button,
-      {
-        backgroundColor: props.backgroundColor,
-        width: props.width,
-        paddingVertical: props.height
-      }]}>
-      <Text
-        style={[styles.text,
+      style={[
+        styles.button,
         {
-          color: props.color,
-          fontSize: props.fontSize,
-          fontWeight: props.bold
-        }]}>{props.title}</Text>
+          backgroundColor: props.backgroundColor,
+          width: props.width,
+          paddingVertical: props.height,
+        },
+      ]}
+    >
+      <Text
+        style={[
+          styles.text,
+          {
+            color: props.color,
+            fontSize: props.fontSize,
+            fontWeight: props.bold,
+          },
+        ]}
+      >
+        {props.title}
+      </Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-export default ButtonCustom
+export default ButtonCustom;
 
 const styles = StyleSheet.create({
   button: {
     borderRadius: 5,
   },
   text: {
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
