d6e172a09a69e9f74f7a3016c2590dfd6730a023
Andi Fahmi <andifahmi266@gmail.com>
2023-08-11 08:41:59 +0800

    Tidy Up

--- Src/Screens/Reels/index.js
+++ Src/Screens/Reels/index.js
@@ -1,41 +1,54 @@
-import { View, Text, Dimensions, TouchableOpacity } from 'react-native'
-import React, { useRef } from 'react'
-import Feather from 'react-native-vector-icons/Feather'
-import { Colors } from '../../Components/Colors';
-import ReelsComponent from '../../Components/ReelsComponent';
+import React, { useRef } from "react";
+import { View, Text, Dimensions, TouchableOpacity } from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import { Colors } from "../../Components/Colors";
+import ReelsComponent from "../../Components/ReelsComponent";
 
 const Reels = () => {
-  const widthWindow = Dimensions.get('window').width;
-  const heightWindow = Dimensions.get('window').height;
+  const widthWindow = Dimensions.get("window").width;
+  const heightWindow = Dimensions.get("window").height;
 
   return (
     <View
       style={{
         width: widthWindow,
         height: heightWindow,
-        backgroundColor: '#fffff',
-        position: 'relative',
-        backgroundColor: Colors.colorBlack
-      }}>
+        backgroundColor: "#fffff",
+        position: "relative",
+        backgroundColor: Colors.colorBlack,
+      }}
+    >
       <View
         style={{
-          position: 'absolute',
+          position: "absolute",
           top: 0,
           left: 0,
           right: 0,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
+          flexDirection: "row",
+          justifyContent: "space-between",
           padding: 10,
-          zIndex: 1
-        }}>
-        <Text style={{ fontSize: 28, fontWeight: 'bold', color: Colors.colorWhite2 }}>Reels</Text>
+          zIndex: 1,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 28,
+            fontWeight: "bold",
+            color: Colors.colorWhite2,
+          }}
+        >
+          Reels
+        </Text>
         <TouchableOpacity>
-          <Feather name='camera' style={{ fontSize: 30, color: Colors.colorWhite2 }} />
+          <Feather
+            name="camera"
+            style={{ fontSize: 30, color: Colors.colorWhite2 }}
+          />
         </TouchableOpacity>
       </View>
       <ReelsComponent />
     </View>
-  )
-}
+  );
+};
 
-export default Reels
+export default Reels;
