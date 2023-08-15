5b72001b4f084af9fc2f828626e4c3978373e5c6
Andi Fahmi <andifahmi266@gmail.com>
2023-08-15 08:43:46 +0800

    Tidy Up

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionic from 'react-native-vector-icons/Ionicons'
-import Home from './Src/Screens/Home';
-import Search from './Src/Screens/Search';
-import Reels from "./Src/Screens/Reels";
+import Home from "./Src/Screens/Home";
 import Route from './Src/Route';
 
 export default function App() {
