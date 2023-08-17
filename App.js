c423f8c2aac079b1aecedfd9da7cf3f6a3254429
Andi Fahmi <andifahmi266@gmail.com>
2023-08-17 08:44:20 +0800

    Tidy Up

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
 import Route from './Src/Route';
 
 export default function App() {
